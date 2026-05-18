a67d538dc5d09f7f5c1d206a018a003bb898180e
cvx1806 <gtvkhoa2008@gmail.com>
2026-05-19 02:21:00 +0700

    Restore enable_model_summary in pl.Trainer

--- train.py
+++ train.py
@@ -117,7 +117,6 @@ def main() -> None:
         log_every_n_steps=10,
         deterministic=False,
         accumulate_grad_batches=cfg["solver"].get("grad_accumulation", 1),
-        enable_model_summary=False,
     )
 
     trainer.fit(model, datamodule=dm, ckpt_path=args.resume)
